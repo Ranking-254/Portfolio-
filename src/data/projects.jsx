18d450833a199b9cfd4ba93257a2488a3f507780
Ranking-254 <pattinnjue@gmail.com>
2025-11-28 14:55:54 +0300

    added cinema plus project

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,6 +1,6 @@
 import jobhunt from '../assets/jobhunt.webp';
 import portfolio from '../assets/portfolio.webp';
-import agrismart from '../assets/agrismart.webp';
+import Cinemaplus from '../assets/Cinemaplus.webp';
 
 const projects = [
     {
@@ -21,11 +21,11 @@ const projects = [
     },
     {
         id: 3,
-        title: 'AgriSmart - Marketplace',
-        summary: 'Farm produce marketplace MVP for local farmers.',
-        tech: ['React', 'Stripe'],
-        link: '#',
-        image: agrismart
+        title: 'Cinema plus+',
+        summary: 'Cinema Plus+ is aFull stack project with a modern, responsive platform for showcasing events and enabling professional bookings with ease.',
+        tech: ['React', 'Vite', 'Clerk auth','Tailwind/CSS', 'Vercel and render hosting', 'modern JavaScript','mongodb'],
+        link: 'https://cinema-plus-delta.vercel.app/',
+        image: Cinemaplus
     },
 ]
 
